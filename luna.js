bf7dee9f416368c58b214b9e7deffb79439460cb
心叶 <yelloxing@gmail.com>
2018-02-03 16:00:17 +0800

    ###第二部分：复合的单选择器

--- luna.js
+++ luna.js
@@ -3,7 +3,7 @@
     个人内部使用的小型js工具库
     心叶
     V0.0.1-alpha
-    最后修改于：2018-02-02
+    最后修改于：2018-02-03
 ====================================
 */
 (function(global, factory, undefined) {
@@ -706,79 +706,6 @@
 (function(window, Luna, undefined) {
     'use strict';
 
-    Luna.extend({
-        "sizzle": function(selector, context) {
-            selector = selector.trim();
-            var resultData = [],
-                flag, elems;
-
-            //第0部分：选择全部
-            if (selector == '*') {
-                elems = context.getElementsByTagName('*');
-                for (flag = 0; flag < elems.length; flag++) {
-                    resultData.push(elems[flag]);
-                }
-            }
-            //第一部分：最单纯的选择器
-            else if (Luna._sizzle_.isSingle(selector)) {
-                if (Luna._sizzle_.isID(selector)) {
-                    //id选择器 上下文只能是HTML文档，考虑id的唯一性，直接全局查找
-                    //浏览器支持情况：IE 6+, Firefox 3+, Safari 3+, Chrome 4+, and Opera 10+；
-                    resultData.push(document.getElementById(selector.replace(/^#/, "")));
-                } else if (Luna._sizzle_.isClass(selector)) {
-                    //class选择器 上下文可以是HTML文档，XML文档及元素节点
-                    if (context.getElementsByClassName) {
-                        //浏览器支持情况：IE 9+, Firefox 3+, Safari4+, Chrome 4+, and Opera 10+；
-                        elems = context.getElementsByClassName(selector.replace(/^./, ""));
-                        for (flag = 0; flag < elems.length; flag++) {
-                            resultData.push(elems[flag]);
-                        }
-                    } else {
-                        elems = context.getElementsByTagName('*');
-                        for (flag = 0; flag < elems.length; flag++) {
-                            if (Luna(elems[flag]).hasClass(selector.replace(/^./, ""))) {
-                                resultData.push(elems[flag]);
-                            }
-                        }
-                    }
-                } else if (Luna._sizzle_.isElemment(selector)) {
-                    //元素选择器 上下文可以是HTML文档，XML文档及元素节点
-                    elems = context.getElementsByTagName(selector);
-                    for (flag = 0; flag < elems.length; flag++) {
-                        resultData.push(elems[flag]);
-                    }
-                } else if (Luna._sizzle_.isAttr(selector)) {
-                    if (!context.querySelectorAll) {
-                        // 浏览器支持情况：IE 8+, Firefox 3.5+, Safari 3+, Chrome 4+, and Opera 10+；
-                        // 上下文可以是HTML文档，XML文档及元素节点
-                        elems = context.querySelectorAll(selector);
-                        for (flag = 0; flag < elems.length; flag++) {
-                            resultData.push(elems[flag]);
-                        }
-                    } else {
-                        elems = context.getElementsByTagName('*');
-                        var selector_exec = /^\[([^=]+)=(["'])([^=]+)\2\]$/.exec(selector);
-                        for (flag = 0; flag < elems.length; flag++) {
-                            if (selector_exec[3] == Luna(elems[flag]).attr(selector_exec[1])) {
-                                resultData.push(elems[flag]);
-                            }
-                        }
-                    }
-                } else {
-                    throw new Error('非法的选择器:' + selector);
-                }
-            }
-            //第二部分：复合的单选择器
-            else if (Luna._sizzle_.notLayer(selector)) {
-                resultData.push('第二部分：复合的单选择器   开发中');
-            }
-            //第三部分：关系选择器 【'>',"空","~","+"】
-            else {
-                resultData.push('第三部分：关系选择器 【' > ',"空","~","+"】     开发中');
-            }
-            return resultData;
-        }
-    });
     Luna.extend(Luna._sizzle_, {
         "notLayer": function(selector) {
             // 判断是否是关系选择器，只可以确定不是关系选择器
@@ -846,7 +773,180 @@
             } else {
                 return false;
             }
+        },
+        "isValidComplex": function(selector) {
+            //判断是不是合法的第二类选择器 前置条件：已经知道只可能是第二类选择器或者非法
+            selector = selector.replace(/^[^#.[]+/, ''); //去掉开头的标签选择器
+            selector = selector.replace(/\[([^=]+)=(["'])([^=]+)\2\]/g, ''); //去掉合法的属性选择器
+            selector = selector.replace(/#[^#.[]+/g, ''); //去掉id选择器
+            selector = selector.replace(/\.[^#.[]+/g, ''); //去掉class选择器
+            if (selector != "") { //如果此时还存在，一定是非法的
+                return false;
+            } else {
+                return true;
+            }
+        },
+        "toComplexSelectorObject": function(selector) {
+            //前置条件：已经知道确定是第二类选择器
+            var selectorObj = {},
+                flag;
+            var currentSelector = selector.match(/^[^#.[]+/); //标签
+            if (!!currentSelector && currentSelector.length > 0) {
+                selectorObj._elem_ = currentSelector[0];
+            }
+            currentSelector = selector.match(/\[([^=]+)=(["'])([^=]+)\2\]/g); //属性选择器
+            if (!!currentSelector && currentSelector.length > 0) {
+                selectorObj._attr_ = [];
+                for (flag = 0; flag < currentSelector.length; flag++) {
+                    selectorObj._attr_.push(currentSelector[flag]);
+                }
+            }
+            currentSelector = selector.match(/#[^#.[]+/g); //id选择器
+            if (!!currentSelector && currentSelector.length > 0) {
+                selectorObj._id_ = (currentSelector[0]).replace(/^#/, "");
+            }
+            currentSelector = selector.match(/\.[^#.[]+/g); //class选择器
+            if (!!currentSelector && currentSelector.length > 0) {
+                selectorObj._class_ = [];
+                for (flag = 0; flag < currentSelector.length; flag++) {
+                    selectorObj._class_.push(currentSelector[flag].replace(/^\./, ""));
+                }
+            }
+            return selectorObj;
+        },
+        "checkedElems": function(needCheckResultArray, selectorObj) {
+            //id选择器不用匹配了，如果有一定会用，不用就是错误
+            var flag, resultData = [],
+                innerFlag, tempClass, selector_exec,
+                isAccept;
+            for (flag = 0; flag < needCheckResultArray.length; flag++) {
+                isAccept = true;
+                if (!!selectorObj._elem_ && isAccept) { //1.检测元素类型
+                    if (needCheckResultArray[flag].tagName != (selectorObj._elem_ + "").toUpperCase()) {
+                        isAccept = false;
+                    }
+                }
+                if (!!selectorObj._class_ && selectorObj._class_.length > 0 && isAccept) { //2.检测class
+                    tempClass = needCheckResultArray[flag].getAttribute('class') || '';
+                    tempClass = " " + tempClass + " ";
+                    for (innerFlag = 0; innerFlag < selectorObj._class_.length && isAccept; innerFlag++) {
+                        if (tempClass.search(" " + selectorObj._class_[innerFlag] + " ") < 0) {
+                            isAccept = false;
+                        }
+                    }
+                }
+                if (!!selectorObj._attr_ && selectorObj._attr_.length > 0 && isAccept) { //3.检测attr
+                    for (innerFlag = 0; innerFlag < selectorObj._attr_.length && isAccept; innerFlag++) {
+                        var selector_exec = /^\[([^=]+)=(["'])([^=]+)\2\]$/.exec(selectorObj._attr_[innerFlag]);
+                        if (needCheckResultArray[flag].getAttribute(selector_exec[1]) != selector_exec[3]) {
+                            isAccept = false;
+                        }
+                    }
+                }
+                if (isAccept) { //通过全部检测就接受
+                    resultData.push(needCheckResultArray[flag]);
+                }
+            }
+            return resultData;
         }
     });
+
+    Luna.extend({
+        "sizzle": function(selector, context) {
+            selector = selector.trim();
+            var resultData = [],
+                flag, elems;
+
+            //第0部分：选择全部
+            if (selector == '*') {
+                elems = context.getElementsByTagName('*');
+                for (flag = 0; flag < elems.length; flag++) {
+                    resultData.push(elems[flag]);
+                }
+            }
+            //第一部分：最单纯的选择器
+            else if (Luna._sizzle_.isSingle(selector)) {
+                if (Luna._sizzle_.isID(selector)) {
+                    //id选择器 上下文只能是HTML文档，考虑id的唯一性，直接全局查找，因此id选择器不支持上下文查找，各种类型都一样
+                    //浏览器支持情况：IE 6+, Firefox 3+, Safari 3+, Chrome 4+, and Opera 10+；
+                    resultData.push(document.getElementById(selector.replace(/^#/, "")));
+                } else if (Luna._sizzle_.isClass(selector)) {
+                    //class选择器 上下文可以是HTML文档，XML文档及元素节点
+                    if (context.getElementsByClassName) {
+                        //浏览器支持情况：IE 9+, Firefox 3+, Safari4+, Chrome 4+, and Opera 10+；
+                        elems = context.getElementsByClassName(selector.replace(/^./, ""));
+                        for (flag = 0; flag < elems.length; flag++) {
+                            resultData.push(elems[flag]);
+                        }
+                    } else {
+                        elems = context.getElementsByTagName('*');
+                        for (flag = 0; flag < elems.length; flag++) {
+                            if (Luna(elems[flag]).hasClass(selector.replace(/^./, ""))) {
+                                resultData.push(elems[flag]);
+                            }
+                        }
+                    }
+                } else if (Luna._sizzle_.isElemment(selector)) {
+                    //元素选择器 上下文可以是HTML文档，XML文档及元素节点
+                    elems = context.getElementsByTagName(selector);
+                    for (flag = 0; flag < elems.length; flag++) {
+                        resultData.push(elems[flag]);
+                    }
+                } else if (Luna._sizzle_.isAttr(selector)) {
+                    if (!context.querySelectorAll) {
+                        // 浏览器支持情况：IE 8+, Firefox 3.5+, Safari 3+, Chrome 4+, and Opera 10+；
+                        // 上下文可以是HTML文档，XML文档及元素节点
+                        elems = context.querySelectorAll(selector);
+                        for (flag = 0; flag < elems.length; flag++) {
+                            resultData.push(elems[flag]);
+                        }
+                    } else {
+                        elems = context.getElementsByTagName('*');
+                        var selector_exec = /^\[([^=]+)=(["'])([^=]+)\2\]$/.exec(selector);
+                        for (flag = 0; flag < elems.length; flag++) {
+                            if (selector_exec[3] == Luna(elems[flag]).attr(selector_exec[1])) {
+                                resultData.push(elems[flag]);
+                            }
+                        }
+                    }
+                } else {
+                    throw new Error('invalid selector1:' + selector);
+                }
+            }
+            //第二部分：复合的单选择器
+            else if (Luna._sizzle_.notLayer(selector)) {
+                if (Luna._sizzle_.isValidComplex(selector)) {
+                    var complexSelectorObj = Luna._sizzle_.toComplexSelectorObject(selector);
+                    var needCheckResultArray = [];
+                    if (!!complexSelectorObj._id_) { //如果存在id选择器
+                        needCheckResultArray = [document.getElementById(complexSelectorObj._id_)];
+                        return Luna._sizzle_.checkedElems(needCheckResultArray, complexSelectorObj);
+                    } else if (!!complexSelectorObj._elem_) { //如果存在elem
+                        needCheckResultArray = context.getElementsByTagName(complexSelectorObj._elem_);
+                        return Luna._sizzle_.checkedElems(needCheckResultArray, complexSelectorObj);
+                    } else if (!!complexSelectorObj._class_ && complexSelectorObj._class_.length > 0) {
+                        if (context.getElementsByClassName) { //如果是class
+                            //浏览器支持情况：IE 9+, Firefox 3+, Safari4+, Chrome 4+, and Opera 10+；
+                            needCheckResultArray = context.getElementsByClassName(complexSelectorObj._class_[0]);
+                            return Luna._sizzle_.checkedElems(needCheckResultArray, complexSelectorObj);
+                        }
+                    } else { //如果没办法提前过滤，就检测全部
+                        needCheckResultArray = context.getElementsByTagName('*');
+                        return Luna._sizzle_.checkedElems(needCheckResultArray, complexSelectorObj);
+                    }
+                } else {
+                    throw new Error('invalid selector2:' + selector);
+                }
+            }
+            //第三部分：关系选择器 【'>',"空","~","+"】
+            else if (false) {
+                resultData.push('第三部分：关系选择器 【' > ',"空","~","+"】     开发中');
+            } else {
+                throw new Error('undesigned selector:' + selector);
+            }
+            return resultData;
+        }
+    });
+
 
 })(window, window.Luna);
