a9cff251b842b644dd3667375d55a2efda5bd3e7
心叶 <yelloxing@gmail.com>
2018-02-08 09:08:37 +0800

    更新版本号（测试版本0.0.2）

--- luna.js
+++ luna.js
@@ -1,4 +1,4 @@
-/*! luna-library alpha0.0.1 | (c) 2007, 2018 yelloxing | MIT git+https://github.com/yelloxing/Luna.js.git */
+/*! luna-library alpha0.0.2 | (c) 2007, 2018 yelloxing | MIT git+https://github.com/yelloxing/Luna.js.git */
 (function(global, factory, undefined) {
     'use strict';
 
